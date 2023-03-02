d00e84dc226e2bb713a0986888279d24bd81975d
Sonia Justiniano <sjustinianoayte@gmail.com>
2023-03-01 20:23:02 -0500

    n

--- lib/pages/avatar_page.dart
+++ lib/pages/avatar_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class AbatarPage extends StatelessWidget {
+class AvatarPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold();
